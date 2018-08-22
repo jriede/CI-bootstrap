bbdc8eeb54eda121e359f7badf4c430ef5eea1dd
Julia Dressler <jule@knitting.today>
2018-08-22 21:15:44 +0200

    Updated to last ci & bs versions

--- application/logs/log-2018-08-22.php
+++ application/logs/log-2018-08-22.php
@@ -4,3 +4,8 @@ ERROR - 2018-08-22 19:08:38 --> Severity: Notice --> Undefined variable: _SESSIO
 ERROR - 2018-08-22 19:09:04 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
 ERROR - 2018-08-22 19:09:04 --> Severity: error --> Exception: Call to undefined function asset_url() /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 10
 ERROR - 2018-08-22 19:11:38 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-22 19:13:41 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-22 19:13:56 --> 404 Page Not Found: Login/index
+ERROR - 2018-08-22 19:14:00 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-22 19:15:06 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-22 19:15:12 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
