a2ff0740eb35a9ca94adcbb1225f5f7aaeb8e20a
Julia Dressler <jule@knitting.today>
2018-08-22 21:12:10 +0200

    Updated to latest version of ci

---
+++ application/logs/log-2018-08-22.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-08-22 19:08:38 --> Severity: Notice --> Undefined variable: _SESSION /Users/jule/Sites/CI-bootstrap/application/views/welcome_message.php 17
+ERROR - 2018-08-22 19:09:04 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-22 19:09:04 --> Severity: error --> Exception: Call to undefined function asset_url() /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 10
+ERROR - 2018-08-22 19:11:38 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
