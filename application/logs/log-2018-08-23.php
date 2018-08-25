e3e941f8f2f2d1112fc08ac1f08ed9c9e1b864fc
Julia Dressler <jule@knitting.today>
2018-08-25 15:38:18 +0200

    Added octicons

---
+++ application/logs/log-2018-08-23.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-08-23 17:40:43 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-23 17:40:52 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
+ERROR - 2018-08-23 17:40:54 --> Severity: Notice --> Undefined variable: title /Users/jule/Sites/CI-bootstrap/application/views/templates/header.php 7
